67cf3249f87bab5082913dc23754cb3d18fdb52e
Will <william.bath@btconnect.com>
2019-11-04 14:24:43 +0000

    Completed basic HTML framework for Index and List Space

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require 'sinatra/base'
 class MakersBNB < Sinatra::Base
 
   get '/' do
-    'Welcome to Makers BNB!'
+    erb (:index)
+  end
+
+  get '/list_space' do
+    erb (:list_space)
+  end
+
+  post '/' do
+    'Thank you for listing your space!'
   end
 
 end
