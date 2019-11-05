1a5f1addc1cd91ee0d70a9c8dbecce653c945534
josie <josieopjam@gmail.com>
2019-11-05 10:07:51 +0000

    added fields to listings

--- db/migrations/create_makersbnb.sql
+++ db/migrations/create_makersbnb.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE "makersbnb";
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT, cost int, address TEXT, description TEXT);
--- db/migrations/create_makersbnb_test.sql
+++ db/migrations/create_makersbnb_test.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE "makersbnb_test";
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT, cost int, address TEXT, description TEXT);
