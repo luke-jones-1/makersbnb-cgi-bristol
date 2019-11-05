b01a8684fd7eba16c8d5422f2a0b610f5b21b02f
Will <william.bath@btconnect.com>
2019-11-05 14:13:01 +0000

    Starting signUpInterface

--- spec/features/list_space_spec.rb
+++ spec/features/list_space_spec.rb
@@ -9,6 +9,9 @@ feature 'List Space' do
     visit('/')
     click_button('List a New Space')
     fill_in('name', with: 'My lovely cottage')
+    fill_in('cost', with: 80)
+    fill_in('address', with: '34 Madeup Lane')
+    fill_in('description', with: 'A quaint and pretty cottage in the middle of nowhere.')
     click_button('Submit')
     expect(page).to have_content 'Thank you for listing your space!'
   end
