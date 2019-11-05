013cd90fb8818db7a87eebda475755c036792123
TobiasBenjaminYoungCGI <toby.young@cgi.com>
2019-11-05 14:30:15 +0000

    Pg changes and more

--- src/User.js
+++ src/User.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function User() {
-
+  // const { Client } = require('pg')
+  // const client = new Client()
+  // await client.connect()
+  // const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+  // console.log(res.rows[0].message) // Hello world!
+  // await client.end()
 }
 
 User.prototype.create() {
