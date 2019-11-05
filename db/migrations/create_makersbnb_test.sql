06d933d666350fab737acea80f00936675aa5e81
josie <josieopjam@gmail.com>
2019-11-05 10:32:44 +0000

    added user table

--- db/migrations/create_makersbnb_test.sql
+++ db/migrations/create_makersbnb_test.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE "makersbnb_test";
+
 CREATE TABLE listings(id SERIAL PRIMARY KEY, name TEXT, cost decimal (18 ,2), address TEXT, description TEXT);
+
+CREATE TABLE users(id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, email TEXT, password TEXT);
