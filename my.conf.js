6dba64f05d06aa849c33af5971211465784bfd21
Luke Jones <lukejonesice@yahoo.co.uk>
2019-11-06 09:39:28 +0000

    travis actually running jasmine test

--- my.conf.js
+++ my.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/*.js, spec/*Spec.js'
+      './src/*.js, ./spec/*Spec.js'
     ],
 
 
